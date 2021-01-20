3e88bd5abcc156a1ba3368afc192fdd3bd6e36af
Rucha Patil <ruchapatil216@gmail.com>
2021-01-20 14:44:45 +0530

    doc updated

---
+++ JavaProgramms/CollectionExpDemo.java
@@ -0,0 +1,22 @@
+import java.util.*;
+class A{}
+class B{}
+ public class CollectionExpDemo
+ {
+   ArrayList al=new ArrayList();
+   al.add(0);
+   al.add(10.10);
+    al.add("ram");
+	 al.add(true);
+	 A obj1=new A();
+	 al.add(obj1);
+	 B obj2=new B();
+	 al.add(obj2);
+	 System.out.println(al);
+	 
+ }
+   
+   
+ 
+ 
+ 
