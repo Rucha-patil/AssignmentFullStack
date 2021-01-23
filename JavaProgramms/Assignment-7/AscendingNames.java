1e50c044f8d3698082ef69a0fd8a925b42271045
Rucha Patil <ruchapatil216@gmail.com>
2021-01-23 10:22:08 +0530

    change some code

--- JavaProgramms/Assignment-7/AscendingNames.java
+++ JavaProgramms/Assignment-7/AscendingNames.java
@@ -1,17 +1,15 @@
 import java.util.Arrays;
 import java.util.Collections;
 
+
 public class AscendingNames
 {
 	public static void main(String args[])
 	{
-		String names[]={"Amrut","ajay","Aaditi","Riya","Bablu"};
+		String names[]={"Raj","ravi","Akash","ajay"};
 		
-		int size = names.length;
-		
-		Arrays.sort(names);
 		//Ascending Order//
-		Arrays.sort(names, String.CASE_INSENSITIVE_ORDER);
+		Arrays.sort(names);
         System.out.println("Ascending Order : ");
 		System.out.println(Arrays.toString(names));
 		
@@ -19,7 +17,10 @@ public class AscendingNames
 		Arrays.sort(names, Collections.reverseOrder());
 		System.out.println("Descending Order : ");
 		System.out.println(Arrays.toString(names));
-		
+	
 
 	}
+	
 }
+
+
